15f91e9b124fadf9cd4994c1603405395c9a6011
Sophie Senge <sophieperks@gmail.com>
2023-01-28 01:55:02 +0000

    added variables for target city current points

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+// add event listener with fetch values
+fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=d79ebdf8167debecd6d65d2247f86bad")
+.then(response => response.json())
+.then(citiesFound =>{
+  let firstCity = citiesFound[0];
+  console.log(firstCity.lat);
+  console.log(firstCity.lon);
+
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=d79ebdf8167debecd6d65d2247f86bad&units=metric`)
+})
+
+.then(response => response.json())
+.then(data => {
+  console.log(data)
+  //values which need to be used in main weather div
+  cityName = data.city.name;
+  console.log(data.city.name)
+  //needs to be current date
+  cityDate = data.city.timezone;
+  console.log(data.city.timezone)
+  cityTemp = data.list[0].main.temp
+  console.log(data.list[0].main.temp)
+  console.log(data.list[0].wind.speed)
+  console.log(data.list[0].main.humidity)
+    
+})
+
+
+
+
+
+
+/** The city name
+    * The date
+    * An icon representation of weather conditions
+    * The temperature
+    * The humidity
+    * The wind speed*/
